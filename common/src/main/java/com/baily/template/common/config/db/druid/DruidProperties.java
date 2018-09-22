6245620db43a7150929595f1e090c4f71efa7963
Baily <yinbah@163.com>
2018-09-22 19:39:51 +0800

    增加lombok引用依赖

--- common/src/main/java/com/baily/template/common/config/db/druid/DruidProperties.java
+++ common/src/main/java/com/baily/template/common/config/db/druid/DruidProperties.java
@@ -1,10 +1,28 @@
 package com.baily.template.common.config.db.druid;
 
+import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
 
-@ConfigurationProperties
-@EnableConfigurationProperties
+@ConfigurationProperties(prefix = "druid.config")
+@Data
 public class DruidProperties {
+
+    private String urlMapping;
+
+    private String resetEnabl;
+
+    private String allow;
+
+    private String deny;
+
+    private String loginUsername;
+
+    private String loginPassword;
+
+    private String urlPatterns;
+
+    private String exclusions;
+
+    private String profileEnable;
+
 }
