b0adec71787bafd5e132fceae4d925b4775e032b
opg-yin-beibei <yin.beibei@opg.cn>
2018-10-12 17:43:46 +0800

    整合apollo配置中心

--- zuul-gate/src/main/java/com/baily/template/demo/spring/SpringController.java
+++ zuul-gate/src/main/java/com/baily/template/demo/spring/SpringController.java
@@ -2,11 +2,11 @@ package com.baily.template.demo.spring;
 
 import com.ctrip.framework.apollo.spring.annotation.ApolloJsonValue;
 import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @RestController
@@ -15,6 +15,9 @@ public class SpringController {
 
     @Value("${http.connectTimeout:2000}")
     long connectTimeout;
+
+    @Autowired
+    TestBean testBean;
 
     /**
      * jsonBeanProperty=[{"someString":"hello","someInt":100},{"someString":"world!","someInt":200}]
@@ -30,6 +33,13 @@ public class SpringController {
     @RequestMapping(value = "getJsonBeans", method = RequestMethod.GET)
     public String getJsonBeans() {
         return jsonBeans.toString();
+    }
+
+    @RequestMapping(value = "getProperty", method = RequestMethod.GET)
+    public String getJsonBeans(HttpServletRequest request) {
+        String result = testBean.getEnvValue(request.getQueryString());
+        System.out.println(result);
+        return "result:"+result;
     }
 
 
